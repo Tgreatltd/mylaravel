3e070f08f955d017e79b16707debcd9fd165dadb
oluwatobi <olamoyegunoluwagreat@gmail.com>
2023-09-09 11:49:30 -0400

    msg

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,4 +3,11 @@
 
 @section('content')
     <h1>welcome</h1>
+    <h1>welcome</h1>
+    <h1>welcome</h1>
+    <h1>welcome</h1>
+    <h1>welcome</h1>
+    <h1>welcome</h1>
+    <h1>welcome</h1>
+    <h1>welcome</h1>
 @endsection
