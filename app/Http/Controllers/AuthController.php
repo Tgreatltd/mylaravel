767b4814484b448cbd9f2f4c251e0c6f7bddda00
oluwatobi <olamoyegunoluwagreat@gmail.com>
2023-09-18 03:56:38 -0400

    ok

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -22,7 +22,6 @@ class AuthController extends Controller
 
         User::create($data);
         User::create($data);
-        User::create($data);
         session()->flash('success','Your account has been registered');
         return Redirect()->back();
     }
