541d576b5a7000894c11991259f4437e02334c25
oluwatobi <olamoyegunoluwagreat@gmail.com>
2023-10-01 08:36:50 -0400

    new commit

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -35,6 +35,7 @@
                 <input type="text" class="form-control" name="password">
                 @error('password')
                     <small class="text-danger">{{$message}}</small>
+                    <small class="text-danger">{{$text}}</small>
                 @enderror
             </div>
             <button name="submit" class="btn btn-success form-control">Submit</button>
