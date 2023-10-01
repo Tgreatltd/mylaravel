ca8fbf4549488003eb0057013d252f15a13598da
oluwatobi <olamoyegunoluwagreat@gmail.com>
2023-10-01 08:36:19 -0400

    new commit

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -11,7 +11,6 @@
             @csrf
             <div class="form-group">
                 <label for="firstName">FirstName</label>
-                <label for="firstName">FirstName</label>
                 <input type="text" class="form-control" name="firstName" value="{{old('firstName')}}">
                 @error('firstName')
                     <small class="text-danger">{{$message}}</small>
