7d1f94cd3e3469e8c380e37dd8854bfcb04bd253
oluwatobi <olamoyegunoluwagreat@gmail.com>
2023-10-02 10:17:41 -0400

    new commit

--- routes/web.php
+++ routes/web.php
@@ -37,7 +37,6 @@ Route::post('/update/{id}', [AuthController::class, 'update'])->name('update');
 Route::get('/del/{id}', [AuthController::class, 'del'])->name('del');
 Route::get('/image', [MyController::class, 'image'])->name('image');
 Route::post('/store', [AuthController::class, 'stores'])->name('stores');
-Route::post('/store', [AuthController::class, 'stores'])->name('stores');
 
 // fallback is used to handle 404 error pages
 Route::fallback(function () {
